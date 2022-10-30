408031b49ecacee838fc4e825f82337a0004a277
Victor Ajagunna <victor-ajagunna@hotmail.com>
2022-10-30 19:13:30 +0100

    project setup

---
+++ server/sampleData.js
@@ -0,0 +1,61 @@
+const projects = [
+    {
+        "id": 1,
+        "clientId": 1,
+        "desc": "Excision of Left Abdomen Muscle, Percutaneous Approach",
+        "status": "In Progress"
+    }, {
+        "id": 2,
+        "clientId": 2,
+        "desc": "Restriction of Right Middle Lobe Bronchus, Open Approach",
+        "status": "In Progress"
+    }, {
+        "id": 3,
+        "clientId": 3,
+        "desc": "Dilation of Intracranial Vein, Open Approach",
+        "status": "In Progress"
+    }, {
+        "id": 4,
+        "clientId": 4,
+        "desc": "Insert Tissue Expander in R Low Leg Subcu/Fascia, Perc",
+        "status": "Done'"
+    }, {
+        "id": 5,
+        "clientId": 5,
+        "desc": "Plain Radiography of L Salivary Gland using L Osm Contrast",
+        "status": "In Progress"
+    },
+];
+
+const clients = [
+  {
+    id: 1,
+    name: "Enrika Capstack",
+    email: "ecapstack0@ehow.com",
+    phone: "851-674-8523",
+  },
+  {
+    id: 2,
+    name: "Maurits Hawley",
+    email: "mhawley1@dagondesign.com",
+    phone: "151-111-3563",
+  },
+  {
+    id: 3,
+    name: "Say Cozby",
+    email: "scozby2@google.cn",
+    phone: "533-132-4312",
+  },
+  {
+    id: 4,
+    name: "Darren Corkhill",
+    email: "dcorkhill3@nps.gov",
+    phone: "551-268-2971",
+  },
+  {
+    id: 5,
+    name: "Ring Wendover",
+    email: "rwendover4@newyorker.com",
+    phone: "562-547-7487",
+  },
+];
